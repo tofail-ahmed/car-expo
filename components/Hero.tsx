cfd50e96d0f2a5d7ade514ba23d1cf7430244dc0
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-12 12:13:47 +0600

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl text-amber-400 font-bold">Hero</h1>
+      <h1 className="text-3xl text-amber-400">This is Hero</h1>
+    </div>
+  );
+};
+
+export default Hero;
