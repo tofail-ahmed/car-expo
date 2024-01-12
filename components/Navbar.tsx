2c2272a374f2e005b6d16538ab51f04b23301639
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-12 21:40:39 +0600

    logo added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <header className='w-full absolute z-10'>
+      <nav className='max-w-[1440px] mx-auto flex justify-between items center sm:px-16 px-6 py-6'>
+            <Link href='/'>
+                  <Image src='/logo.svg' alt="Car expo logo" width={120} height={20} className="object-contain" />
+            </Link>
+      </nav>
+   </header>
   )
 }
 
